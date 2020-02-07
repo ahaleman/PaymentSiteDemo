dad20d1ea22e3d7b2ad9515d0706dd9c6755da9d
Alejandro Herrera <AlejandroHerrera@intramart.com>
2020-02-07 16:26:44 -0600

    Add project files.

---
+++ PaymentSite/Payment.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Payment.aspx.cs" Inherits="PaymentSite.Payment" %>
+
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+<head>
+    <title>HostedPayment Test Page</title>
+    <script src="Scripts/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#add_payment").show();
+            $("#send_token").attr({ "action": "https://secure.authorize.net/payment/payment", "target": "add_payment" }).submit();
+            $(window).scrollTop($('#add_payment').offset().top - 50);
+        });
+    </script>
+</head>
+<body>
+    <h1>Parent Page</h1>
+    <div id="iframe_holder" class="center-block" style="width: 90%; max-width: 1000px; border-color:red; border-width:thick;">
+        <iframe id="add_payment" class="embed-responsive-item panel" name="add_payment" width="100%" frameborder="0" scrolling="no" hidden="true"></iframe>
+    </div>
+    <form id="send_token" action="" method="post" target="add_payment">
+        <input type="hidden" name="token" value="<%=Token%>" />
+    </form>
+
+    <script type="text/javascript">
+        (function () {
+            if (!window.AuthorizeNetIFrame) window.AuthorizeNetIFrame = {};
+            AuthorizeNetIFrame.onReceiveCommunication = function (querystr) {
+                var params = parseQueryString(querystr);
+                switch (params["action"]) {
+                    case "cancel":
+                        console.log("cancel");
+                        console.log(querystr);
+                        break;
+                    case "resizeWindow":
+                        console.log("resizeWindow");
+                        console.log(querystr);
+                        var w = parseInt(params["width"]);
+                        var h = parseInt(params["height"]);
+                        var ifrm = document.getElementById("add_payment");
+                        ifrm.style.width = w.toString() + "px";
+                        ifrm.style.height = h.toString() + "px";
+                        break;
+                    case "transactResponse":
+                        console.log("transactResponse");
+                        console.log(querystr);
+                        var ifrm = document.getElementById("add_payment");
+                        ifrm.style.display = 'none';
+                }
+            };
+
+            function parseQueryString(str) {
+                var vars = [];
+                var arr = str.split('&');
+                var pair;
+                for (var i = 0; i < arr.length; i++) {
+                    pair = arr[i].split('=');
+                    vars.push(pair[0]);
+                    vars[pair[0]] = unescape(pair[1]);
+                }
+                return vars;
+            }
+        }());
+    </script>
+</body>
+</html>
---
+++ PaymentSite/Payment.aspx.cs
@@ -0,0 +1,127 @@
+﻿using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Controllers.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PaymentSite
+{
+    public partial class Payment : System.Web.UI.Page
+    {
+        public string Token { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.PRODUCTION;
+
+                ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+                {
+                    name = "**********",
+                    ItemElementName = ItemChoiceType.transactionKey,
+                    Item = "****************"
+                };
+
+                var settings = new List<settingType>();
+
+                settings.Add(new settingType()
+                {
+                    settingName = settingNameEnum.hostedPaymentReturnOptions.ToString(),
+                    settingValue = "{\"showReceipt\": false}"
+                });
+
+                settings.Add(new settingType()
+                {
+                    settingName = settingNameEnum.hostedPaymentButtonOptions.ToString(),
+                    settingValue = "{\"text\": \"Pay\"}"
+                });
+
+                settings.Add(new settingType()
+                {
+                    settingName = settingNameEnum.hostedPaymentStyleOptions.ToString(),
+                    settingValue = "{\"bgColor\": \"blue\"}"
+                });
+
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentPaymentOptions.ToString(),
+                    settingValue = "{\"cardCodeRequired\": false, \"showCreditCard\": true, \"showBankAccount\": true}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentSecurityOptions.ToString(),
+                    settingValue = "{\"captcha\": false}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentShippingAddressOptions.ToString(),
+                    settingValue = "{\"show\": false, \"required\": false}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentBillingAddressOptions.ToString(),
+                    settingValue = "{\"show\": true, \"required\": false}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentCustomerOptions.ToString(),
+                    settingValue = "{\"showEmail\": false, \"requiredEmail\": false, \"addPaymentProfile\": true}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentOrderOptions.ToString(),
+                    settingValue = "{\"show\": true, \"merchantName\": \"Frisco\"}"
+                });
+
+                settings.Add(new settingType
+                {
+                    settingName = settingNameEnum.hostedPaymentIFrameCommunicatorUrl.ToString(),
+                    settingValue = "{\"url\": \"https://localhost:44376/Communicator.html\"}"
+                });
+
+                var transactionRequest = new transactionRequestType
+                {
+                    transactionType = transactionTypeEnum.authCaptureTransaction.ToString(),    // authorize capture only
+                    amount = 99.99M,
+                    billTo = new customerAddressType
+                    {
+                        firstName = "Alex",
+                        lastName = "Herrera",
+                        company = "Avolve",
+                        address = "123 Main Street",
+                        city = "Phoenix",
+                        state = "Arizona",
+                        zip = "83000"
+                    },
+                    refTransId = "RES-ELECTRIC-000",
+                    poNumber = DateTime.Now.ToString("yyMMddhhmmss"),
+                };
+
+                var request = new getHostedPaymentPageRequest
+                {
+                    transactionRequest = transactionRequest,
+                    hostedPaymentSettings = settings.ToArray(),
+                    refId = "RES-ELECTRIC-000"
+                };
+
+                var controller = new getHostedPaymentPageController(request);
+                controller.Execute();
+
+
+                var response = controller.GetApiResponse();
+
+                Token = response.token;
+            }
+        }
+    }
+}
